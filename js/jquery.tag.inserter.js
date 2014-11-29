f7eb3238c9b08705475cc5cde7e59b4174b557df
Gabriel Schulhof <gabriel.schulhof@intel.com>
2014-11-29 23:53:27 +0200

    Tests: Try to modify tag inserter to work with XHTML

--- js/jquery.tag.inserter.js
+++ js/jquery.tag.inserter.js
@@ -2,26 +2,36 @@
 	/*jshint evil: true */
 	// Insert a script tag pointing at the desired version of jQuery
 
+		debugger;
+
 	// Get the version from the url
 	var jqueryRE = /[\\?&]jquery=([^&#]*)/,
 		results = jqueryRE.exec( location.search ),
 		version = "",
 		myScriptTag = document.getElementsByTagName( "script" )[document.getElementsByTagName( "script" ).length - 1],
 		baseUrl = myScriptTag.src.replace( /(.*)\/.*$/, "$1/" ),
-		url = baseUrl + "../external/jquery/jquery.js";
+		url = baseUrl + "../external/jquery/jquery.js",
+		markup = "",
+		script;
 
 	if ( results ) {
 		version = decodeURIComponent( results[results.length - 1].replace( /\+/g, " " ) );
 		url = "http://code.jquery.com/jquery-" + version + ".js";
 	}
 
-	document.write( "<script src='" + url + "'></script>" );
+	script = document.createElement( "script" );
+	script.setAttribute( "src", url );
+	script.setAttribute( "type", "text/javascript" );
+	document.getElementsByTagName( "head" )[ 0 ].appendChild( script );
+/*
+	markup =
+		"\n\t<script src='" + url + "'></script>\n" +
+		"\t<script type='text/javascript'>\n//<![CDATA[\n" +
+			"\t\tif ( window.jQuery && parseInt( jQuery.fn.jquery.replace( /\\./g, '' ), 10 ) < 170 && window.define && window.define.amd ) {\n" +
+			"\t\t\tdefine( 'jquery', [], function () { return jQuery; } );\n" +
+			"\t\t}\n" +
+			"//]]>\n\t</script>";
 
-	document.write(
-		"<script>" +
-			"if ( window.jQuery && parseInt( jQuery.fn.jquery.replace( /\\./g, '' ), 10 ) < 170 && window.define && window.define.amd ) {" +
-			"define( 'jquery', [], function () { return jQuery; } );" +
-			"}" +
-			"</script>"
-	);
+	document.getElementsByTagName( "head" )[ 0 ].innerHTML += markup;
+*/
 }());
