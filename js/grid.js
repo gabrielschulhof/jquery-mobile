40790312f1536b2498c788015d2f0ef35ca44b8e
Gabriel Schulhof <gabriel.schulhof@intel.com>
2015-03-17 23:43:41 +0200

    Grid: Ignore script tags

--- js/grid.js
+++ js/grid.js
@@ -16,8 +16,9 @@ $.fn.grid = function( options ) {
 			o = $.extend({
 				grid: null
 			}, options ),
-			$kids = $this.children(),
+			$kids = $this.children().not( "script" ),
 			gridCols = { solo:1, a:2, b:3, c:4, d:5 },
+			blockIds = [ "a", "b", "c", "d", "e" ],
 			grid = o.grid,
 			iterator,
 			letter;
@@ -38,20 +39,9 @@ $.fn.grid = function( options ) {
 
 		$this.addClass( "ui-grid-" + grid );
 
-		$kids.filter( ":nth-child(" + iterator + "n+1)" ).addClass( "ui-block-a" );
-
-		if ( iterator > 1 ) {
-			$kids.filter( ":nth-child(" + iterator + "n+2)" ).addClass( "ui-block-b" );
-		}
-		if ( iterator > 2 ) {
-			$kids.filter( ":nth-child(" + iterator + "n+3)" ).addClass( "ui-block-c" );
-		}
-		if ( iterator > 3 ) {
-			$kids.filter( ":nth-child(" + iterator + "n+4)" ).addClass( "ui-block-d" );
-		}
-		if ( iterator > 4 ) {
-			$kids.filter( ":nth-child(" + iterator + "n+5)" ).addClass( "ui-block-e" );
-		}
+		$kids.each( function( index, element ) {
+			$( element ).addClass( "ui-block-" + ( blockIds[ index % iterator ] ) );
+		} );
 	});
 };
 })( jQuery );
